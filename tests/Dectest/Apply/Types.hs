902907b4befb2c715d92d72f342ffcd8b4987710
Omari Norman <omari@smileystation.com>
2014-05-06 17:17:54 -0400

    WIP

--- tests/Dectest/Apply/Types.hs
+++ tests/Dectest/Apply/Types.hs
@@ -1,19 +1,20 @@
 module Dectest.Apply.Types where
 
+import Data.Char (toLower)
 import qualified Data.ByteString.Char8 as BS8
 import qualified Deka.Context as C
 import Data.Functor.Identity
 import Data.List (find)
 import qualified Dectest.Interp.Result as R
 import Dectest.Interp.Operand
-import Dectest.Interp.Octothorpe
+import Dectest.Interp.Octothorpe (WhichPrecision)
+import Data.List (sort)
 
-data Result
+data Bypass
   = Skip
   -- ^ No suitable test for this operation; skip the test
   | Null
   -- ^ One of the operands was null; skip the test
-  | Pass
   | Fail BS8.ByteString [C.Flag]
   -- ^ Comes with the string representation of the test result
   | OperandMismatch
@@ -29,12 +30,60 @@ data Directives a = Directives
   }
 
 type ApplyTest a
-  = [C.Ctx (WhichPrecision -> a)]
+  = [WhichPrecision -> C.Ctx a]
   -- ^ Operands
   -> Maybe (C.Ctx a)
   -- ^ Return Nothing if there is an operand type mismatch;
   -- otherwise, return the result of the function
 
+-- | Given the name of a test and an Alist of test functions, picks
+-- the test to apply.
+pickTestFn
+  :: BS8.ByteString
+  -- ^ Name of property being tested
+  -> [(BS8.ByteString, ApplyTest a)]
+  -- ^ A-list of property names and test functions
+  -> Either Bypass (ApplyTest a)
+pickTestFn n ls = case find pdct ls of
+  Nothing -> Left Skip
+  Just (_, f) -> Right f
+  where
+    pdct (tn, _) = lower tn == lower n
+    lower = map toLower . BS8.unpack
+
+-- | Given a list of operands, parse them.  Bypasses if any of the
+-- operands are null.
+interpOperands
+  :: Operand a
+  => [BS8.ByteString]
+  -> Either Bypass [WhichPrecision -> C.Ctx a]
+interpOperands bss = case mapM operand bss of
+  Nothing -> Left Null
+  Just rs -> return rs
+
+-- | Given a test function and a list of parsed operands, apply the
+-- test function to the operands.  Bypass if there is an operand type
+-- mismatch.
+
+applyOperands
+  :: ApplyTest a
+  -> [WhichPrecision -> C.Ctx a]
+  -> Either Bypass (C.Ctx a)
+applyOperands f = maybe (Left OperandMismatch) return . f
+
+-- | Given a ByteString for the target test result, return a
+-- function that, when applied to the result of applying the test
+-- function, indicates success or failure.
+
+interpResult
+  :: R.Result a
+  => BS8.ByteString
+  -> Either Bypass (a -> C.Ctx Bool)
+interpResult = maybe (Left Null) return . R.result
+
+-- | Given the result of a computation and how to apply it, 
+
+{-
 applyTest
   :: (Operand a, R.Result a, R.ToByteString a)
   => [(BS8.ByteString, ApplyTest a)]
@@ -50,16 +99,18 @@ applyTest
   -- ^ Result and expected status flags
   -> Result
 applyTest ls dir nm os (rslt, fs) = either id id $ do
-  fn <- maybe (Left Skip) return $ find pdct ls
+  let pdct (k, _) = (map toLower . BS8.unpack $ k)
+        == (map toLower . BS8.unpack $ nm)
+  fn <- maybe (Left Skip) (return . snd) $ find pdct ls
   kos <- maybe (Left Null) return $ mapM operand os
-  getR <- maybe (Left Null) return $ result rslt
+  getR <- maybe (Left Null) return $ R.result rslt
   compR <- maybe (Left OperandMismatch) return $ fn kos
   let comp = do
         applyDirectives dir
         n <- compR
         compR n
-      (r, rFl) = runCtxStatus initQuad comp
-      bs = toByteString r
+      (r, rFl) = C.runCtxStatus C.initQuad comp
+      bs = R.toByteString r
       throwFlags | sort rFl == sort fs = Right ()
                  | otherwise = Left (Fail bs rFl)
       throwFail | r = Right ()
@@ -67,5 +118,6 @@ applyTest ls dir nm os (rslt, fs) = either id id $ do
   throwFlags
   throwFail
   return Pass
+-}
 
 applyDirectives = undefined
