ad19b94da97a52e1ac76f6fa19a668985b2894e4
Omari Norman <omari@smileystation.com>
2014-05-09 08:45:10 -0400

    WIP

--- tests/testParse.hs
+++ tests/testParse.hs
@@ -2,9 +2,11 @@ module Main where
 
 import Dectest.Parse
 import System.Environment
+import qualified Data.ByteString.Char8 as BS8
+import Text.Show.Pretty
 
 main :: IO ()
 main = do
   a:_ <- getArgs
-  is <- parseFile a
-  mapM_ putStrLn . map show $ is
+  is <- parseFile . BS8.pack $ a
+  putStrLn . ppShow $ is
