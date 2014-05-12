4a7f91a9219a30599f5a1713d31027f1053841a0
Omari Norman <omari@smileystation.com>
2014-05-12 15:59:58 -0400

    WIP

--- dectest/Runner.hs
+++ dectest/Runner.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings, BangPatterns #-}
-module Runner where
+module Runner (runAndExit) where
 
 import qualified Parse as P
 import qualified Data.ByteString.Char8 as BS8
@@ -11,118 +11,40 @@ import qualified Data.Sequence as S
 import Data.Sequence ((|>))
 import Pipes
 import Pipes.Prelude (fold)
-import qualified Control.Monad.Trans.State as St
 import NumTests (testLookups)
 import Types
-import Control.Arrow (first)
 
-type State = St.StateT (S.Seq (P.Keyword, P.Value))
-type State' = St.StateT (S.Seq (P.Keyword, P.Value), Counts)
+produceFile :: MonadIO m => Pipe BS8.ByteString P.File m ()
+produceFile = do
+  bs <- await
+  f <- liftIO $ P.parseFile bs
+  yield f
 
-produceFile
-  :: MonadIO m
-  => BS8.ByteString
-  -- ^ Filename
-  -> Producer P.File m ()
-produceFile fn = liftIO (P.parseFile fn) >>= yield
-
-produceFiles
-  :: MonadIO m
-  => [BS8.ByteString]
-  -> Producer P.File m ()
-produceFiles bss = for (each bss) produceFile
-
-contents
-  :: Monad m
-  => P.File
-  -> Producer (Either P.File P.Instruction) m ()
-contents f = each (P.fileContents f)
-
-eiToInstructions
-  :: Monad m
-  => Either P.File P.Instruction
-  -> Producer P.Instruction m ()
-eiToInstructions ei = case ei of
-  Right i -> yield i
-  Left f -> for (contents f) eiToInstructions
-
-instructions
-  :: MonadIO m
-  => BS8.ByteString
-  -> Producer P.Instruction m ()
-instructions bs = for (produceFile bs) (eiToInstructions . Left)
-
-instructions' :: Monad m => P.File -> Producer P.Instruction m ()
-instructions' f = for (yield f) (eiToInstructions . Left)
-
-localFile :: MonadIO m => P.File -> m Counts
-localFile f = do
-  let st = runEffect $ for (each (P.fileContents f)) procEither
-      start = (S.empty, mempty)
-  (_, cnts) <- liftIO $ St.execStateT st start
-  return cnts
-
-procEither
-  :: MonadIO m
-  => Either P.File P.Instruction
-  -> Effect (State' m) ()
-procEither ei = case ei of
-  Left file -> do
-    let getEfct = runEffect (localFile file)
-    localCnts <- liftIO getEfct
-    let mdfy (sq, cnts) = (sq, cnts <> localCnts)
-    lift $ St.modify mdfy
-    return ()
-  Right ins -> return ins >~ consumeInstructions
-
-  
-procInstruction
-  :: Monad m
-  => Pipe P.Instruction P.TestSpec (State' m) ()
-procInstruction = do
-  ins <- await
-  case ins of
-    P.Blank -> procInstruction
-    P.Directive k v -> do
-      lift (St.modify (first (|> (k,v))))
-      procInstruction
-    P.Test ts -> yield ts
-
-data TestOutput = TestOutput
-  { outSpec :: P.TestSpec
-  , outResult :: Maybe Bool
-  , outLog :: S.Seq BS8.ByteString
-  }
-
-procSpec :: Monad m => Pipe P.TestSpec TestOutput (State' m) ()
-procSpec = do
-  spc <- await
-  case lookup (P.testOperation spc) testLookups of
-    Nothing -> yield (noOperation spc)
-    Just f -> do
-      dirs <- fmap fst . lift $ St.get
-      yield (runTest dirs spc f)
-
-noOperation :: P.TestSpec -> TestOutput
-noOperation ts = TestOutput
-  { outSpec = ts
-  , outResult = Nothing
-  , outLog = S.singleton "no matching operation; skipping"
-  }
-
-runTest
-  :: S.Seq (P.Keyword, P.Value)
-  -> P.TestSpec
-  -> Test
-  -> TestOutput
-runTest ds ts t = TestOutput
-  { outSpec = ts
-  , outResult = r
-  , outLog = l
-  }
+order :: Monad m => Pipe P.File Order m ()
+order = do
+  f <- await
+  go S.empty (P.fileContents f)
   where
-    (r, l) = t ds (P.testOperands ts) (P.testResult ts)
-      (P.testConditions ts)
+    go !sq ls = case ls of
+      [] -> return ()
+      x:xs -> case x of
+        Left inner -> go S.empty (P.fileContents inner)
+        Right i -> case i of
+          P.Blank -> go sq xs
+          P.Directive k v ->
+            let sq' = sq |> (k, v)
+            in go sq' xs
+          P.Test spec -> do
+            yield (Order sq spec)
+            go sq xs
+
+output :: Monad m => Pipe Order TestOutput m ()
+output = do
+  o <- await
+  case lookup (P.testOperation . ordSpec $ o) testLookups of
+    Nothing -> yield (noOperation . ordSpec $ o)
+    Just t -> yield (runTest o t)
+
 
 printTest
   :: MonadIO m
@@ -132,58 +54,66 @@ printTest = do
   liftIO . BS8.putStr . showResult $ o
   yield (outResult o)
 
-tallySink :: Monad m => Consumer (Maybe Bool) (State' m) ()
-tallySink = do
-  mb <- await
-  (s, cnt) <- lift St.get
-  let !cnt' = tally cnt mb
-  lift $ St.put (s, cnt')
+pipeline :: MonadIO m => Pipe BS8.ByteString (Maybe Bool) m ()
+pipeline =
+  produceFile
+  >-> order
+  >-> output
+  >-> printTest
+
+totals :: Monad m => Producer (Maybe Bool) m () -> m Counts
+totals = fold tally mempty id
+
+runAndExit :: [String] -> IO ()
+runAndExit ss = do
+  let bs = map BS8.pack ss
+      pip = each bs >-> pipeline
+  tot <- totals pip
+  putStr . showCounts $ tot
+  exit tot
+
+noOperation :: P.TestSpec -> TestOutput
+noOperation ts = TestOutput
+  { outSpec = ts
+  , outResult = Nothing
+  , outLog = S.singleton "no matching operation; skipping"
+  }
+
+data Order = Order
+  { _ordDirectives :: S.Seq (P.Keyword, P.Value)
+  , ordSpec :: P.TestSpec
+  }
+
+runTest
+  :: Order
+  -> Test
+  -> TestOutput
+runTest (Order ds ts) t = TestOutput
+  { outSpec = ts
+  , outResult = r
+  , outLog = l
+  }
+  where
+    (r, l) = t ds (P.testOperands ts) (P.testResult ts)
+      (P.testConditions ts)
+
+data TestOutput = TestOutput
+  { outSpec :: P.TestSpec
+  , outResult :: Maybe Bool
+  , outLog :: S.Seq BS8.ByteString
+  }
+
+data Counts = Counts
+  { _nPass :: !Int
+  , nFail :: !Int
+  , _nSkip :: !Int
+  } deriving Show
 
 tally :: Counts -> Maybe Bool -> Counts
 tally (Counts p f s) mb = case mb of
   Nothing -> Counts p f (s + 1)
   Just True -> Counts (p + 1) f s
   Just False -> Counts p (f + 1) s
-
-totals :: Monad m => Producer (Maybe Bool) m () -> m Counts
-totals = fold tally (Counts 0 0 0) id
-
-produceResults
-  :: MonadIO m
-  => [BS8.ByteString]
-  -- ^ Files
-  -> Effect (State' m) ()
-produceResults fs
-  = for (each fs) instructions
-  >-> consumeInstructions
-
-consumeInstructions
-  :: MonadIO m
-  => Consumer P.Instruction (State' m) ()
-consumeInstructions
-  = procInstruction
-  >-> procSpec
-  >-> printTest
-  >-> tallySink
-
-
-runAndExit :: [BS8.ByteString] -> IO ()
-runAndExit = undefined
-{-
-runAndExit bs = do
-  cs <- runEffect $ each 
-  (_, cts) <- St.execStateT
-    (runEffect (produceResults bs)) (S.empty, Counts 0 0 0)
-  putStr . showCounts $ cts
-  exit cts
--}
-
-
-data Counts = Counts
-  { nPass :: !Int
-  , nFail :: !Int
-  , nSkip :: !Int
-  } deriving Show
 
 instance Monoid Counts where
   mempty = Counts 0 0 0
--- dectest/dectest.hs
+++ dectest/dectest.hs
@@ -2,7 +2,6 @@ module Main where
 
 import Runner
 import System.Environment
-import qualified Data.ByteString.Char8 as BS8
 
 main :: IO ()
-main = fmap (map BS8.pack) getArgs >>= runAndExit
+main = getArgs >>= runAndExit
